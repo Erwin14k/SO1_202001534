9b6183e92d212b6cbf40769877866fc76ae18629
Erwin Vásquez <82339728+Erwin14k@users.noreply.github.com>
2023-02-15 20:35:55 -0600

    Script Image Dockerfile
    Script Image Dockerfile

--- Practice_01/script/reportsScript.sh
+++ Practice_01/script/reportsScript.sh
@@ -1,15 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Get today's date in the format used in logs.txt
-today_date=$(date "+%Y-%m-%d")
+# Set the timezone to Guatemala
+export TZ="America/Guatemala"
 
 # Count the total number of lines in the file
 total_lines=$(wc -l logs.txt | cut -d' ' -f1)
 
 # Count the number of occurrences of -1499 in the file
-#total_errors=$(grep -c -- "-,-1499;" logs.txt)
 total_errors=$(grep -c '/,-\?1499\.' logs.txt)
-
 
 # Count the number of occurrences of + in the file
 total_sums=$(grep -c -- "+," logs.txt)
@@ -23,8 +21,12 @@ total_multiplications=$(grep -c -- "\*," logs.txt)
 # Count the number of occurrences of / in the file
 total_divisions=$(grep -c -- "/," logs.txt)
 
-# Count the number of occurrences of today's date in the file
+# Get today's date in the format used in logs.txt
+today_date=$(date "+%Y-%m-%d")
+
+# Count the number of logs for today's date
 total_today=$(grep -c -- "$today_date" logs.txt)
+logs_today=$(grep "$today_date" logs.txt)
 
 # Print the results
 echo "Total logs registered: $total_lines"
@@ -33,4 +35,7 @@ echo "Total Sums: $total_sums"
 echo "Total Subtractions: $total_subtractions"
 echo "Total Multiplications: $total_multiplications"
 echo "Total Divisions: $total_divisions"
+echo $today_date
 echo "Total logs registered today: $total_today"
+
+printf "Logs for today report:\n%s\n" "$logs_today"
